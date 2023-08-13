b01ef9e45017df15cec10afbe3235b4afa14de04
Dawid Lachowicz <domdaw7@gmail.com>
2023-08-13 21:47:28 +0100

    Update generic hello derivation tree with new rules and program example

--- projects/go-generic-array-sizes/hello-generic.go
+++ projects/go-generic-array-sizes/hello-generic.go
@@ -8,6 +8,11 @@ func (this Array[N, T]) Get(i int) T {
 	return this[i]
 }
 
+func (this Array[N, T]) Length() int {
+	return N
+}
+
 func main() {
-	_ = Array[2, int]{1, 2}.Get(0)
+	_ = Array[2, int]{1, 2}.Get(0)   // 1
+	_ = Array[2, int]{1, 2}.Length() // 2
 }
