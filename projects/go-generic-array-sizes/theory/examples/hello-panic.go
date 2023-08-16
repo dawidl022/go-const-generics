566068cb9f61e5f12e0d0be23392ba256c5d6d0c
Dawid Lachowicz <domdaw7@gmail.com>
2023-08-16 20:16:57 +0100

    Organise theory work in subdirectory

--- projects/go-generic-array-sizes/theory/examples/hello-panic.go
+++ projects/go-generic-array-sizes/theory/examples/hello-panic.go
@@ -3,9 +3,6 @@ package main
 type AnyArray0 [0]any
 
 func (this AnyArray0) First() any {
-	// when monomorphising, any constants indexing into a generic
-	// should be first converted into non-constant ints
-	//
 	// this[0] would cause a compile-time error, whereas the below code panics
 	index := 0
 	return this[index]
