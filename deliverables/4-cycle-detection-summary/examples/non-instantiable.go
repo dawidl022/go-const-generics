3e149519adcfe53a759dc22bf615101606eca50b
Dawid Lachowicz <domdaw7@gmail.com>
2024-08-17 16:11:51 +0100

    Add counter examples to simply removing notRef

---
+++ deliverables/4-cycle-detection-summary/examples/non-instantiable.go
@@ -0,0 +1,21 @@
+package main
+
+// Let's take an example of types that cannot be instantiated, yet the current
+// compiler implementation permits it.
+//
+// If we cannot instantiate Foo, when it recursively refers to struct type Bar,
+// then we would encounter the same issue with a constraint of the
+// form a Foo[a]. The compiler currently rejects the latter case due to
+// the notReferenced rule.
+//
+// If the goal of cycle detection is to prevent declaring non-instantiable types,
+// then the rules should be ammended to prevent scenarios such as the below.
+type Foo[a Bar[a]] struct {
+	x a
+}
+
+type Bar[b any] struct {
+}
+
+func main() {
+}
