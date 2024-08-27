d0833352f0f69b288a6406b9e1c72937b565a5c4
Dawid Lachowicz <domdaw7@gmail.com>
2024-08-27 09:09:57 +0100

    Derive positive example in self-ref types

--- deliverables/4-cycle-detection-summary/examples/self-ref-valid.go
+++ deliverables/4-cycle-detection-summary/examples/self-ref-valid.go
@@ -3,11 +3,11 @@ package main
 type Foo[F Foo[F]] interface{ m(f F) F }
 type Bar[B Bar[B]] interface{ m(b B) B }
 
+type E[F Foo[F], B Bar[B]] struct{}
+
 type T struct{}
 
 func (t T) m(t2 T) T { return t }
-
-type E[F Foo[F], B Bar[B]] struct{}
 
 func main() {
 	_ = E[T, T]{}
